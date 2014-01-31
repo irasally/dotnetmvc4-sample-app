8bc535ce7a2177100c978186357221b71eb423ff
irasally <irasally@gmail.com>
2014-01-31 14:32:53 +0900

    get categories from menu action.

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo){
+            repository = repo;
+        }
+        public PartialViewResult Menu()
         {
-            return "Hello from NavController";
+            IEnumerable<string> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
+            return PartialView(categories);
         }
 
     }
