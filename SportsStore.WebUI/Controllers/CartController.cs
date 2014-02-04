9e04c60bac5150e11448291c16dc021df48004e3
irasally <irasally@gmail.com>
2014-02-04 18:07:45 +0900

    created cart index view

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -15,6 +16,15 @@ namespace SportsStore.WebUI.Controllers
         public CartController(IProductRepository repo)
         {
             repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
         }
 
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
