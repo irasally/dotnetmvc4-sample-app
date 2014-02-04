ed6616bcf269837f15569878acc1d483195a4161
irasally <irasally@gmail.com>
2014-02-04 16:50:37 +0900

    added adding new item test.

---
+++ SportsStore.UnitTests/CartTest.cs
@@ -0,0 +1,84 @@
+﻿using SportsStore.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    
+    
+    /// <summary>
+    ///CartTest のテスト クラスです。すべての
+    ///CartTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class CartTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "p1" };
+            Product p2 = new Product { ProductID = 2, Name = "p2" };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] actual = target.Lines.ToArray();
+
+            Assert.AreEqual(actual.Length, 2);
+            Assert.AreEqual(actual[0].Product, p1);
+            Assert.AreEqual(actual[1].Product, p2);
+        }
+    }
+}
