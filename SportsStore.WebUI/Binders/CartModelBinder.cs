45ec5e98b88de1f24692f0d185b299a4fce891b6
irasally <irasally@gmail.com>
2014-02-05 15:41:11 +0900

    edit binder model.

--- SportsStore.WebUI/Binders/CartModelBinder.cs
+++ SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Binders
 {
-    public class CartModelBinder
+    public class CartModelBinder : IModelBinder
     {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            return cart;
+        }
     }
 }
