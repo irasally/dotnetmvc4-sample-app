add9fe335c81b359e9c308108ea865818913392d
irasally <irasally@gmail.com>
2014-02-06 17:03:40 +0900

    starting over with the AdminController class.

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
     }
 }
