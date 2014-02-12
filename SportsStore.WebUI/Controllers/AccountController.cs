e277e9a3421c9c9f269f292f133789a4994deda3
irasally <irasally@gmail.com>
2014-02-12 15:58:24 +0900

    Added Logon Controller

---
+++ SportsStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthProvider authProvider;
+
+        public void AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+
+        public ViewResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                bool logOnSuccess = authProvider.Authenticate(model.UserName, model.Password);
+                if (logOnSuccess)
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
